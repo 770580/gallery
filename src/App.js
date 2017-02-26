5e1dc7f0fb180772bdea5d9c8c35ea149709acfd
Anton <lascivolin@gmail.com>
2017-02-26 12:06:14 +0300

    slider on props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import Slider from './components/Slider';
 import './App.css';
 
+const images = [
+  'http://ru2.anyfad.com/items/t1@bd097733-3fd8-43cf-b554-9ac41c1fe2d1/Bolshaya-belaya-akula.jpg',
+  'http://urbanzen.org/wp-content/uploads/2013/06/Global-Water-Dance.jpg',
+  'http://image.prntscr.com/image/f44dd8e9274444d68efc246784255a90.jpg'
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Slider />
+        <Slider images={images} />
       </div>
     );
   }
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.images = this.props.images;
+    this.currentSlider = 0;
+  }
+
+  previousSlider() {
+    const newCurrentSlider = this.currentSlider - 1;
+    this.currentSlider = ( newCurrentSlider < 0 ) ? 
+      this.images.length - 1 :
+      newCurrentSlider;
+    this.forceUpdate();
+  }
+
+  nextSlider() {
+    const newCurrentSlider = this.currentSlider + 1;
+    this.currentSlider = ( newCurrentSlider > this.images.length - 1 ) ? 
+      0 :
+      newCurrentSlider;
+    this.forceUpdate();
+  }
+
   render() {
     return (
       <div className="Slider">
-        <img src="./img/slide-1.jpg" width="680" height="410" alt="slide" />
-        <button className="Slider__button Slider__button--previous" type="button">&#60;</button>
-        <button className="Slider__button Slider__button--next" type="button">&#62;</button>
+        <img src={this.images[this.currentSlider]} width="680" height="410" alt="slide" />
+        <button onClick={this.previousSlider.bind(this)} className="Slider__button Slider__button--previous" type="button">&#60;</button>
+        <button onClick={this.nextSlider.bind(this)} className="Slider__button Slider__button--next" type="button">&#62;</button>
       </div>
     );
   }
